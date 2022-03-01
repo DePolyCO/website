5bb282cb7a42fab4fdc2187a2c373c9c3a76f002
siddharthsham <siddhsham@gmail.com>
2022-03-01 18:37:07 +0530

    add preload hero items

--- web/src/js/preload.js
+++ web/src/js/preload.js
@@ -3,9 +3,9 @@ export const assets = {
     // "/static/1.jpg",
     // "/static/2.jpg",
     "/static/home/hero.jpg",
+    "/static/about/hero.jpg",
+    "/static/careers/hero.jpg",
+    "/static/technology/hero.jpg",
   ],
-  fonts: [
-    "sans",
-    "sans-serif"
-  ]
+  fonts: ["sans", "sans-serif"],
 };

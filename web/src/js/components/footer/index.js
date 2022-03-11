5ae7cef16b82935f3df6175ea1c80ab86e92ae37
siddharthsham <siddhsham@gmail.com>
2022-03-11 23:17:15 +0530

    add mailchimp integration

--- web/src/js/components/footer/index.js
+++ web/src/js/components/footer/index.js
@@ -1,4 +1,4 @@
-import { qs } from "../../hermes";
+import { iris, qs } from "../../hermes";
 import { FooterHover } from "./footerHover";
 
 import { FooterSlider } from "./footerSlider";
@@ -9,11 +9,38 @@ export class Footer {
 
     this.slider = new FooterSlider({ dom: this.dom });
     this.hover = new FooterHover({ dom: this.dom });
+
+    this.form = qs("form", this.dom);
   }
 
-  init() {
+  init = () => {
     this.slider.init();
-  }
+
+    iris.add(this.form, "submit", this.handleSubmit, { passive: false });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const email = qs("#footer-email", this.form).value;
+    const name = qs("#footer-name", this.form).value;
+
+    this.post(email, name);
+  };
+
+  post = async (email, name) => {
+    const res = await fetch("/.netlify/functions/form-handler", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        name,
+      }),
+    });
+
+    const data = await res.json();
+
+    console.log("data from function", data);
+  };
 }
 
 export const footer = new Footer();

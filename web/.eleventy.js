3dad28490f9045935a6125c769ecdea259ec28d4
siddharthsham <siddhsham@gmail.com>
2022-03-17 22:38:30 +0530

    add lunr / improve blog

--- web/.eleventy.js
+++ web/.eleventy.js
@@ -8,6 +8,37 @@ const fs = require("fs");
 
 require("dotenv").config();
 
+const extractExcerpt = (article) => {
+  if (!article.hasOwnProperty("templateContent")) {
+    console.warn(
+      'Failed to extract excerpt: Document has no property "templateContent".'
+    );
+    return null;
+  }
+
+  let excerpt = null;
+  const content = article.templateContent;
+
+  // The start and end separators to try and match to extract the excerpt
+  const separatorsList = [
+    // { start: "<!-- Excerpt Start -->", end: "<!-- Excerpt End -->" },
+    { start: "<p>", end: "</p>" },
+  ];
+
+  separatorsList.some((separators) => {
+    const startPosition = content.indexOf(separators.start);
+    const endPosition = content.indexOf(separators.end);
+
+    if (startPosition !== -1 && endPosition !== -1) {
+      excerpt = content
+        .substring(startPosition + separators.start.length, endPosition)
+        .trim();
+      return true; // Exit out of array loop on first match
+    }
+  });
+  return excerpt;
+};
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("static");
 
@@ -85,6 +116,8 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addNunjucksShortcode("fileUrlFor", (file) => {
     return buildFileUrl(file);
   });
+
+  eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
 
   eleventyConfig.addFilter("slugify", (input) => {
     if (!input) {
---
+++ web/lambda/search.js
@@ -0,0 +1,53 @@
+const lunrjs = require("lunr");
+
+const handler = async (event) => {
+  try {
+    const search = event.queryStringParameters.term;
+    if (!search) throw "Missing term query parameter";
+
+    const data = require("./article-index.json");
+    const index = createIndex(data);
+
+    let results = index.search(search);
+
+    results.forEach((r) => {
+      r.title = data[r.ref].title;
+      r.content = truncate(data[r.ref].content, 400);
+      r.date = data[r.ref].date;
+      r.url = data[r.ref].url;
+
+      delete r.matchData;
+      delete r.ref;
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+function createIndex(posts) {
+  return lunrjs(function () {
+    this.ref("id");
+    this.field("title");
+    this.field("content");
+    this.field("date");
+
+    posts.forEach((p, idx) => {
+      p.id = idx;
+      this.add(p);
+    });
+  });
+}
+
+function truncate(str, size) {
+  //first, remove HTML
+  str = str.replace(/<.*?>/g, "");
+  if (str.length < size) return str;
+  return str.substring(0, size - 3) + "...";
+}
+
+module.exports = { handler };

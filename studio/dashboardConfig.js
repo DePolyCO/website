7983147e5ae81ba92ed660577b0006e90827d807
siddharthsham <siddhsham@gmail.com>
2022-04-01 23:59:11 +0530

    update build hooks

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -13,13 +13,13 @@ export default {
           {
             title: "Website FRA",
             apiId: "535e62b7-3a8b-4272-89f8-cd7182f2d1bd",
-            buildHookId: "6236cf1b1ce5761bd5865b22",
+            buildHookId: "62474426e9940501915e0fff",
             name: "depoly-fra",
           },
           {
             title: "Website ENG",
             apiId: "8951ec5c-8774-4db0-bfa5-dfd41d6644d9",
-            buildHookId: "6236cf1b1ce5761bd5865b22",
+            buildHookId: "6247444bc474557e3f7f11d6",
             name: "depoly",
           },
         ],

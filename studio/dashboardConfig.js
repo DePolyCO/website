3f57d72a292847d21e30a4b3634ff48d90ee5e93
siddharthsham <siddhsham@gmail.com>
2022-04-01 23:33:46 +0530

    update build hooks

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -9,11 +9,16 @@ export default {
     {
       name: "netlify",
       options: {
-        // title: "My Netlify deploys",
         sites: [
           {
-            title: "Website",
-            apiId: "ff63d67c-e980-493c-ac5d-341dc0f0d65e",
+            title: "Website FRA",
+            apiId: "535e62b7-3a8b-4272-89f8-cd7182f2d1bd",
+            buildHookId: "6236cf1b1ce5761bd5865b22",
+            name: "depoly-fra",
+          },
+          {
+            title: "Website ENG",
+            apiId: "8951ec5c-8774-4db0-bfa5-dfd41d6644d9",
             buildHookId: "6236cf1b1ce5761bd5865b22",
             name: "depoly",
           },

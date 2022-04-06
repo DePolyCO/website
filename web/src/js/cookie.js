5183bce4d16bd2461428c1eceac60bef3a5cfe5c
siddharthsham <siddhsham@gmail.com>
2022-04-06 21:19:24 +0530

    improve cookie

--- web/src/js/cookie.js
+++ web/src/js/cookie.js
@@ -1,8 +1,15 @@
 import { qs, iris, Duration } from "./hermes";
-import { Store } from "./store";
 
 export class Cookie {
   constructor() {
+    const choice = localStorage.getItem("cookieChoice");
+    if (choice && choice.length) {
+      if (choice === "true") {
+        this.startAnalytics();
+      }
+      return;
+    }
+
     new Duration({
       duration: 3000,
       complete: this.init,
@@ -24,27 +31,31 @@ export class Cookie {
   accept = () => {
     this.dom.classList.remove("on");
 
-    // load analytics
+    this.startAnalytics();
+
+    localStorage.setItem("cookieChoice", true);
+
+    this.destroy();
+  };
+
+  startAnalytics = () => {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
       window.dataLayer.push(arguments);
     }
     gtag("js", new Date());
     gtag("config", "G-9KSCSKLDB5");
-
-    Store.cookie = true;
-
-    this.destroy();
   };
 
   decline = () => {
     this.dom.classList.remove("on");
+    localStorage.setItem("cookieChoice", false);
     this.destroy();
   };
 
   destroy = () => {
-    this.unAccept();
-    this.unDecline();
+    this.unAccept && this.unAccept();
+    this.unDecline && this.unDecline();
     this.dom = null;
   };
 }

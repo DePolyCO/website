5a675d1874845c85dbc55b1e758af1e18298b454
Vince Juric <vince.nightshift@gmail.com>
2022-07-05 16:50:41 +0200

    Added `Instagram` to social networks.
    * Updated `contact` and `_footer` templates to include the newly added social network.

--- web/src/views/_data/social.js
+++ web/src/views/_data/social.js
@@ -2,4 +2,5 @@ module.exports = {
   tw: "https://twitter.com/DePolySA",
   fb: "https://www.facebook.com/depoly.sa",
   li: "https://www.linkedin.com/company/depoly/",
+  in: "https://www.instagram.com/depoly.sa",
 };

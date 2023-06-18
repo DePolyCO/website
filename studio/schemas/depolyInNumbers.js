29a95addbcf4dd2a53c500d9b2b2f4ea4197a703
Vince Juric <vince.nightshift@gmail.com>
2023-06-18 23:48:37 +0200

    feat(studio): Added `depolyInNumbers` schema.

---
+++ studio/schemas/depolyInNumbers.js
@@ -0,0 +1,33 @@
+export default {
+  name: "depolyInNumbers",
+  title: "DePoly In Numbers",
+  type: "document",
+  i18n: false,
+  fields: [
+    {
+      name: "employees",
+      title: "Employees",
+      type: "string",
+    },
+    {
+      name: "nationalities",
+      title: "Nationalities",
+      type: "string",
+    },
+    {
+      name: "genderRatio",
+      title: "Gender Ratio",
+      type: "string",
+    },
+    {
+      name: "languages",
+      title: "Languages",
+      type: "string",
+    },
+    {
+      name: "yearsOfExperience",
+      title: "Years in Academia, R&D and Industry",
+      type: "string",
+    },
+  ]
+};
